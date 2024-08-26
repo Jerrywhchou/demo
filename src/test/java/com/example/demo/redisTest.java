6bcb8897f1b3d41850758bf9dfb1882a3055f1b9
Richard <huizhou419398@hotmail.com>
2024-08-26 22:25:11 +0800

    改为采用savebatch来插入数据

---
+++ src/test/java/com/example/demo/redisTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
+
+@Component
+public class redisTest {
+
+    @Test
+    public void test() {}
+
+}
